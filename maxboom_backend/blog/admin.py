0473a0bebdaf81d23f3d9c597585956ec6792c69
Alimov Rinat <r-alimov@ya.ru>
2023-09-10 21:03:51 +0300

    app Blog with models and admin panel

--- maxboom_backend/blog/admin.py
+++ maxboom_backend/blog/admin.py
@@ -1,3 +1,87 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Category, Tag, Post, PostTag
+
+
+class PostTagInLine(admin.TabularInline):
+    """
+    Инлайн для постов и тегов.
+    """
+    model = PostTag
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Админка для постов.
+    """
+    def image_preview(self, obj):
+        return format_html(
+            '<img src="{}" style="max-width:50px; max-height:50px"/>'.format(
+                obj.image.url))
+
+    image_preview.short_description = 'Изображение'
+
+    list_display = (
+        'pk',
+        'pub_date',
+        'title',
+        'text',
+        'image_preview',
+        'category',
+        'author',
+        'slug',
+        'meta_title',
+        'meta_description',
+    )
+    list_filter = (
+        'author',
+        'title',
+        'category',
+        'tags',
+    )
+    inlines = (PostTagInLine,)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Админка для категорий.
+    """
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'meta_title',
+        'meta_description',
+    )
+    list_filter = (
+        'title',
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """
+    Админка для тегов.
+    """
+    list_display = (
+        'name',
+    )
+    list_filter = (
+        'name',
+    )
+
+
+@admin.register(PostTag)
+class PostTagAdmin(admin.ModelAdmin):
+    """
+    Админка для постов и тегов.
+    """
+    list_display = (
+        'pk',
+        'post',
+        'tag',
+    )
--- maxboom_backend/blog/models.py
+++ maxboom_backend/blog/models.py
@@ -1,3 +1,129 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    """
+    Модель категории постов.
+    """
+    title = models.CharField(
+        max_length=250)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name='Слаг')
+    meta_title = models.CharField(
+        verbose_name='Мета-название страницы',
+        max_length=255,
+        blank=True,
+        null=True)
+    meta_description = models.CharField(
+        verbose_name='Мета-описание страницы',
+        max_length=255,
+        blank=True,
+        null=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+    def __str__(self) -> str:
+        return self.title[:15]
+
+
+class Tag(models.Model):
+    """
+    Модель тегов.
+    """
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['id']
+
+    def __str__(self) -> str:
+        return self.name[:15]
+
+
+class Post(models.Model):
+    """
+    Модель для постов.
+    """
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=255)
+    text = models.TextField(
+        verbose_name='Текст')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор')
+    pub_date = models.DateField(
+        verbose_name='Дата публикации',
+        auto_now_add=True)
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='blog/',
+        blank=True,
+        null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        blank=True,
+        null=True)
+    tags = models.ManyToManyField(
+        Tag,
+        through='PostTag',
+        verbose_name='Тег')
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name='Слаг')
+    meta_title = models.CharField(
+        verbose_name='Мета-название страницы',
+        max_length=255,
+        blank=True,
+        null=True)
+    meta_description = models.CharField(
+        verbose_name='Мета-описание страницы',
+        max_length=255,
+        blank=True,
+        null=True)
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['id']
+
+    def __str__(self) -> str:
+        return self.title[:15]
+
+
+class PostTag(models.Model):
+    """
+    Промежуточная модель тегов и постов.
+    """
+    post = models.ForeignKey(
+        Post,
+        verbose_name='Пост',
+        on_delete=models.CASCADE)
+    tag = models.ForeignKey(
+        Tag,
+        verbose_name='Тег',
+        on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Пост и тег'
+        verbose_name_plural = 'Посты и теги'
+        ordering = ['id']
+
+    def __str__(self) -> str:
+        return f'{self.post} с тегом {self.tag}'
