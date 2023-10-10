67b422b67d07a2b166f397eec78ffef3e8564ce6
Alimov Rinat <r-alimov@ya.ru>
2023-10-10 20:57:45 +0300

    extended_schema for news

--- maxboom_backend/api/views/news_views.py
+++ maxboom_backend/api/views/news_views.py
@@ -3,7 +3,14 @@ from rest_framework import viewsets
 from api.serializers.news_serializers import NewsSerializer
 from news.models import News
 
+from drf_spectacular.utils import extend_schema
 
+
+@extend_schema(
+        description='Получение списка всех доступных новостей,\n'
+        'либо одной новости по slug (только безопасные запросы).',
+        responses={200: NewsSerializer(many=True)},
+    )
 class NewsViewSet(viewsets.ReadOnlyModelViewSet):
     """
     Вьюсет только для SAFE methods к новостям.
