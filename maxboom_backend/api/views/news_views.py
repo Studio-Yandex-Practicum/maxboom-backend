baa15a894833ddafb8985529089e8ee762c2710f
Alimov Rinat <r-alimov@ya.ru>
2023-10-11 20:33:39 +0300

    Extended schema for News app is completed

--- maxboom_backend/api/views/news_views.py
+++ maxboom_backend/api/views/news_views.py
@@ -1,19 +1,57 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 from api.serializers.news_serializers import NewsSerializer
 from news.models import News
 
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import (extend_schema,
+                                   extend_schema_view,
+                                   OpenApiParameter,
+                                   OpenApiResponse)
 
 
 @extend_schema(
-        description='Получение списка всех доступных новостей,\n'
-        'либо одной новости по slug (только безопасные запросы).',
-        responses={200: NewsSerializer(many=True)},
+    tags=['Shopnews'],
+    description='Эндпоинт для доступа к новостям',
+)
+@extend_schema_view(
+    list=extend_schema(
+        summary='Получить список новостей',
+        responses={
+            status.HTTP_200_OK: NewsSerializer(many=True),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                description='Страница не найдена.')
+        },
+        parameters=[
+            OpenApiParameter(
+                name='page',
+                location=OpenApiParameter.QUERY,
+                description='Номер страницы',
+                required=False,
+                type=int
+            ),
+        ],
+    ),
+    retrieve=extend_schema(
+        summary='Получить отдельную новость',
+        responses={
+            status.HTTP_200_OK: NewsSerializer,
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                description='Страница не найдена.')
+        },
+        parameters=[
+            OpenApiParameter(
+                name='slug',
+                location=OpenApiParameter.PATH,
+                description='slug новости',
+                required=True,
+                type=str
+            ),
+        ]
     )
+)
 class NewsViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Вьюсет только для SAFE methods к новостям.
+    Вьюсет только для SAFE methods для доступа к новостям.
     """
 
     queryset = News.objects.all()
